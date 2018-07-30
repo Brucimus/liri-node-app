53907746f0036007c911bba8328b7d8eb64be294
Brucimus <Brucimus@gmail.com>
2018-07-29 18:46:48 -0700

    install request and figured out spotify key

--- liri.js
+++ liri.js
@@ -2,6 +2,32 @@ require("dotenv").config();
 
 var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
+var keysMod = require('./keys');
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var spotifyWithKey = new Spotify(keysMod.spotify);
+// var client = new Twitter(keys.twitter);
+
+var action = process.argv[2];
+var value = process.argv[3];
+
+switch (action) {
+    case "my-tweets":
+    console.log("twitter");
+    //   total();
+      break;
+    
+    case "spotify-this-song":
+    console.log("spotify");
+    //   deposit();
+      break;
+    
+    case "movie-this":
+    console.log("this movie");
+    //   withdraw();
+      break;
+    
+    case "do-what-it-says":
+    console.log("do what it says");
+    //   lotto();
+      break;
+    }
